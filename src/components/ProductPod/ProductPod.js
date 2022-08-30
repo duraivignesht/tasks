3fed4575eea1c284733fda914fcc6ddcb7a8c1f5
duraivignesht <112183070+duraivignesht@users.noreply.github.com>
2022-08-30 19:24:07 +0530

    Update ProductPod.js

--- src/components/ProductPod/ProductPod.js
+++ src/components/ProductPod/ProductPod.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
-
+import React from 'react';
 import { Price } from '../Price/Price';
+import { ProductImage } from '../ProductImage/ProductImage';
 import './ProductPod.scss'
+import PropTypes from 'prop-types';
 
 export const ProductPod = ({ product,handleClick }) => {
   if (!product) return null;
@@ -12,12 +13,7 @@ export const ProductPod = ({ product,handleClick }) => {
   return (
     <article className='product-pod' onClick={() => { getSingleProduct(product); }}>
       {/* TODO: make DRY with a ProductImage component */}
-      {product.image && 
-        <img
-          src={product.image} 
-          alt="Home Depot logo"
-        />
-      } 
+      <ProductImage img={{url:product.image,title:'Home Depot logo'}} />
       {product.brand && 
         <b>
           {product.brand}
@@ -32,3 +28,13 @@ export const ProductPod = ({ product,handleClick }) => {
     </article>
   );
 };
+
+ProductPod.propTypes = {
+  product:PropTypes.shape({
+    image:PropTypes.string,
+    brand:PropTypes.string,
+    description:PropTypes.string,
+    price:PropTypes.number
+  }),
+  handleClick:PropTypes.func
+}
