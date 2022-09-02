563ca57e6f2aa51b6acaa5b4b1b81dea12ebd831
duraivignesht <112183070+duraivignesht@users.noreply.github.com>
2022-09-02 11:03:49 +0530

    Update ProductPreview.js

--- src/components/ProductPreview/ProductPreview.js
+++ src/components/ProductPreview/ProductPreview.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Price } from '../Price/Price';
 import './ProductPreview.scss'
-
+import { ProductImage } from '../ProductImage/ProductImage';
+import PropTypes from 'prop-types';
 
 export const ProductPreview = ({ product }) => {
+  const [showSpecialPrice, setshowSpecialPrice] = useState({
+    showSpecialPrice: false
+});
   if (!product) return null;
-
   return (
     <article className="product-preview">
       {/* TODO: make DRY with a ProductImage component */}
       <div className="prodimg">
-
-      {product.image && 
-        <img
-          src={product.image} 
-          alt="Home Depot logo"
-        />
-      } </div>
+      <ProductImage img={{url:product.image,title:'Home Depot logo'}} />
+      </div>
       <aside className="product-preview-details">
         {product.brand && 
           <p>
@@ -31,8 +29,17 @@ export const ProductPreview = ({ product }) => {
             {product.description}
           </p>
         }
-        <Price product={product} showSpecialPrice />
+        <p className='prodclick' onClick={()=>setshowSpecialPrice({ showSpecialPrice: true})}>Show Special Price</p>
+        <Price product={product} {...showSpecialPrice} />
       </aside>
     </article>
   )
 };
+ProductPreview.propTypes = {
+  product:PropTypes.shape({
+    image:PropTypes.string,
+    brand:PropTypes.string,
+    description:PropTypes.string,
+    price:PropTypes.number
+  })
+}
