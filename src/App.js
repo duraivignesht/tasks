1057778f277dcaad46a6a5c4451520d9d4db2615
duraivignesht <112183070+duraivignesht@users.noreply.github.com>
2022-09-13 10:58:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,28 @@
-import React, {useEffect,useState, useReducer} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
-import { Home } from './Home'
-import { ProductPreviewDetail } from './components/ProductPreview/ProductPreviewDetail';
-import {Routes, Route} from "react-router-dom";
+import { Header } from './components/Header/Header'
+import { ProductGrid } from './components/ProductGrid/ProductGrid';
+import { ProductPreview } from './components/ProductPreview/ProductPreview';
 import { getProducts } from './api';
-import { ShopCart } from './components/Cart/ShopCart';
-
-
-export const context = React.createContext();
-
-export const ADD_PRODUCT = "ADD_PRODUCT";
-
-const addProductToCarts = (product, state) => {
-  const updatedCart = [...state.cart];
-  updatedCart.push({ ...product});
-  console.log("up",updatedCart)
-  return { ...state, cart: updatedCart };
-};
-
-export const shopReducer = (state, action) => {
-  switch (action.type) {
-    case ADD_PRODUCT:
-      return addProductToCarts(action.product, state);
-    default:
-      return state;
-  }
-};
 
 function App() {
-
-  useEffect(() => {
+ useEffect(() => {
     getProducts().then(
     	(data)=>setData(data)
     )
 },[]);
 	
-
   const [data, setData] = useState();
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const getProductPreview = useCallback((val) => {
+    setSelectedProduct(val);
+  },[]);
 
-  const [cartState, dispatch] = useReducer(shopReducer, { cart: [] });
-
-  const addProductToCart = product => {
-    dispatch({ type: ADD_PRODUCT, product: product });
-  };
   return (
     <div className="App">
-      <context.Provider value={{
-        products: data?.products,
-        cart: cartState.cart,
-        addProductToCart: addProductToCart
-      }
-        }>
-      <Routes>
-        <Route path="/" exact element={<Home datas={data} />} />
-        <Route path="/productdetail/:id" exact element={<ProductPreviewDetail singleProduct={data?.products} />} />
-        <Route path="/cart" exact element={<ShopCart />} />
-      </Routes>
-      </context.Provider>
-
-      
+      <Header text={data?.headerText} />
+      <ProductGrid products={data?.products} handleClick={getProductPreview} />
+      <ProductPreview key={selectedProduct?.description} product={selectedProduct} />
     </div>
   );
 }
